7e2257aef0969bc71768e0400f036e7ce317eb4b
Gabriela mora <marian.morag@usantoto.edu.co>
2021-03-23 18:08:21 -0500

    Upload 2021
    Upload 2021

---
+++ EXERCIES_JAVA/P1T37_IAVA_METHODS1/src/usta/fis/Main.java
@@ -0,0 +1,26 @@
+package usta.fis;
+/*AUTHOR     :Marian Gabriela Mora Guerrero
+  DATE       :23 March of 2021
+  DESCRIPTION: This software use methods in your code
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        p_show_info_program();
+        p_show_info_program();
+        p_show_info_program();
+        p_show_info_program();
+
+    }
+
+     public static void p_show_info_program(){
+         //DATE   :23 March of 2021
+         // DESCRIPTION: This method show the info software
+         System.out.println("------------------------------------");
+         System.out.println("SOFTMETHOD VERSION 1.0--------------");
+         System.out.println("MAKER: Marian Gabriela Mora Guerrero");
+         System.out.println("-DESCRIPTION: Use methods-----------");
+         System.out.println("-------------------------------------");
+        }
+    }
+
