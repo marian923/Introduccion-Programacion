6ad6f94556db2e0ab34ae02df04d7e10e1ae58a4
Gabriela mora <marian.morag@usantoto.edu.co>
2021-03-23 19:10:59 -0500

    Upload 2021
    Upload 2021

---
+++ EXERCIES_JAVA/P1T36_System_out_print()/src/usta/fis/Main.java
@@ -0,0 +1,37 @@
+package usta.fis;
+//AUTHOR: Marian Gabriela Mora Guerrero
+//DATE: 2021-March-16
+//DESCRIPTION:This is for display an original figure on the screen
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("--------$$$$");
+	System.out.println("------$$$$$$$$$");
+	System.out.println("------$$$$$$$-$");
+	System.out.println("-----$$$$$$$$$$");
+	System.out.println("-------$$$$$$$$$$");
+	System.out.println("------$$$$$$-$$$$$");
+	System.out.println("----$$$$$$$-----$$$");
+	System.out.println("----$$$$$$$-----$");
+	System.out.println("----$$$$$$$$$$");
+	System.out.println("-----$$$$$$$$$$");
+	System.out.println("-----$$$$$$$$$$$");
+	System.out.println("------$$$$$$$$$$$");
+	System.out.println("-$$$$---$$$$$$$$$");
+	System.out.println("--$$$$$$$$$$$$$$$");
+	System.out.println("-$$$$$$$$$$$$$$$");
+	System.out.println("--$$$$$$$$$$$$$");
+	System.out.println("$$$$$$$$$$$$$");
+	System.out.println("--$--$$$$$$");
+	System.out.println("----$$$$$$");
+	System.out.println("----$$$$$");
+	System.out.println("---$$$$$$-----$");
+	System.out.println("---$$$$$$---$$-$$");
+	System.out.println("----$$$$$---$--$$");
+	System.out.println("----$$$$$------$$");
+	System.out.println("-----$$$$$----$$$");
+	System.out.println("-------$$$$$$$$$");
+	System.out.println("---------$$$$");
+
+    }
+}
