81abeae0cdc782c9e0c190112843c5d74a3e2df5
Gabriela mora <marian.morag@usantoto.edu.co>
2021-05-03 09:57:56 -0500

    Upload 2021
    Upload 2021

--- EXERCIES_JAVA/P1T46_Fibonacci/src/usta/fis/Main.java
+++ EXERCIES_JAVA/P1T46_Fibonacci/src/usta/fis/Main.java
@@ -7,22 +7,22 @@ public class Main {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Welcome to Fibonacci Software. Please, insert the limit of dates that you want see of Fibonacci serie:");
 		int v_limit = keyboard.nextInt();
-		while (v_limit <= 0) {
-			System.err.println("Remember, the number should be greater than 0. Please, input the number again.");
-			v_limit = keyboard.nextInt();
-		}
-		int[] v_fibonacci = new int[v_limit];
-
-		v_fibonacci[0] = 1;
-		v_fibonacci[1] = 1;
-
-		for (int i = 2; i < v_limit; i++) {
-			v_fibonacci[i] = v_fibonacci[i - 1] + v_fibonacci[i - 2];
-		}
-
+		int[] v_fibonacci = new int[100];
+		v_fibonacci= serie_fibonacci(v_limit);
 		System.out.println("Fibonacci Series upto " + v_limit);
-		for (int i = 0; i < v_limit; i++) {
-			System.out.print(v_fibonacci[i] + " ");
+		for(int i=0;v_fibonacci[i]!=-1;i++){
+			System.out.print(v_fibonacci[i]+ ", ");
 		}
+
+	}
+	public static int []serie_fibonacci(int v_limit){
+		int v_fibonacci[] = new int[100];
+		v_fibonacci[0] = 0;
+		v_fibonacci[1] = 1;
+		for (int i=1; (v_fibonacci[i-1]+v_fibonacci[i]) < v_limit;i++){
+			v_fibonacci[i+1] = v_fibonacci[i-1] + v_fibonacci[i];
+			v_fibonacci[i+2] = -1;
+		}
+		return v_fibonacci;
 	}
 }
