90a8630e9a83891b1f7e7f8f99e31e17048f1d76
Gabriela mora <marian.morag@usantoto.edu.co>
2021-05-04 19:25:58 -0500

    Upload 2021
    Upload 2021

--- EXERCIES_JAVA/P1T47_Fibonacci_While/src/usta/fis/Main.java
+++ EXERCIES_JAVA/P1T47_Fibonacci_While/src/usta/fis/Main.java
@@ -5,32 +5,39 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Welcome to Fibonacci Software. Please, insert the limit of dates that you want see of Fibonacci series:");
+        int i=0;
+        p_show_info_program();
+        System.out.println("Welcome to Fibonacci Software. Please, insert the limit number that you want to see in Fibonacci series:");
         int v_limit = keyboard.nextInt();
-        System.out.println("Fibonacci Series to " + v_limit);
-        series_fibonacci(v_limit);
+
+        int[] v_fibonacci =p_fibonacci_series(v_limit);
+        System.out.println("Fibonacci Series up to " + v_limit);
+        while(v_fibonacci[i]!=-1) {
+            if (v_fibonacci[i + 1] != -1) {
+                System.out.print(v_fibonacci[i] + ", ");
+            } else {
+                System.out.print(v_fibonacci[i]);
+            }
+            i++;
+        }
     }
 
-    public static int[] series_fibonacci(int v_limit) {
+    public static int[] p_fibonacci_series(int v_limit) {
         int[] v_fibonacci = new int[100];
-        v_fibonacci[0] = 0;
-        v_fibonacci[1] = 1;
-        int i = 1;
-        int j = 0;
+        v_fibonacci[0] = 0; v_fibonacci[1] = 1; int i = 1; int j = 0;
         while ((v_fibonacci[i - 1] + v_fibonacci[i]) < v_limit) {
             v_fibonacci[i + 1] = v_fibonacci[i - 1] + v_fibonacci[i];
             v_fibonacci[i + 2] = -1;
             i++;
         }
-        while (v_fibonacci[j] != -1) {
-            if (j < i) {
-                System.out.print(v_fibonacci[j] + ", ");
-            }
-            if (j == i) {
-                System.out.print(v_fibonacci[j]);
-            }
-            j++;
-        }
         return v_fibonacci;
+    }
+
+    public static void p_show_info_program() {
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("         SoftFibonacciV1.1         ");
+        System.out.println("   Maker: Marian Gabriela Mora .   ");
+        System.out.println("       Date: May / 03 / 2021       ");
+        System.out.println("╚═════════════════════════════════╝");
     }
 }
