fa04b969c78bf162e6379e1d61eaabeacef6af18
Gabriela mora <marian.morag@usantoto.edu.co>
2021-05-05 12:02:42 -0500

    Upload 2021
    Upload 2021

--- EXERCIES_JAVA/P1T43_Calculator_Switch/src/usta/fis/Main.java
+++ EXERCIES_JAVA/P1T43_Calculator_Switch/src/usta/fis/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         p_show_info_program();
         Scanner keyboard = new Scanner(System.in);
-        int v_operation= 0;
+        int v_operation;
         int v_number_one= 0 ;
         int v_number_two= 0 ;
         do {
             System.out.println("Please write one to add two to subtract three to multiply four to divide and five to determine the prime of N numbers and six to calculate the above prime numbers ");
             v_operation= keyboard.nextInt();
-            if (v_operation!=0) {
+            if (v_operation!=0) { //si el numero es distinto a 0 va a iniciar el programa
                 System.out.println("Please enter the first number");
                 v_number_one = keyboard.nextInt();
                 if (v_operation > 0 && v_operation < 5) {
@@ -22,7 +22,10 @@ public class Main {
                 }
             }
             //operate
-            System.out.println("El resultado es:" + p_operate_numbers(v_number_one, v_number_two, v_operation));
+             String v_total_number= p_operate_numbers(v_number_one, v_number_two, v_operation);
+            if (v_operation>=1&&v_operation<=4){
+                System.out.println("The result is: "+v_total_number+".");
+            }
         } while (v_operation!=0);
 
 
@@ -31,7 +34,7 @@ public class Main {
     public static String p_operate_numbers( int v_number_one, int v_number_two, int v_operation){
         Scanner keyboard = new Scanner(System.in);
         int v_total_number=0;
-        String v_convert="";
+        String v_convert;
         switch (v_operation) {
             case 1 : v_total_number = v_number_one + v_number_two;
             break;
@@ -47,30 +50,31 @@ public class Main {
             break;
             case 5 : v_convert = "NO es un number primo";
                     if (p_validate_prime(v_number_one)){
-                        v_convert= "Es number primo";
+                        v_convert= "Es un numero primo";
                     }
+                    System.out.println(v_convert);
             break;
             case 6:
-                v_convert = " Los numeros primos son:"+p_prime_numbers(v_number_one);
+                System.out.println(p_prime_numbers(v_number_one));
+
             default :
                 v_convert= "Exit";
+                System.out.println(v_convert);
                 System.exit(0);
         }
-        if(v_convert.isEmpty()){
-            v_convert=String.valueOf(v_total_number);
-        }
-        System.out.println("The result is: "+v_total_number+".");
+
+
         return Integer.toString(v_total_number);
         }
 
         public static String p_prime_numbers(int v_limit){
-        String prime_numbers="";
-        for(int counter = 1; counter<=v_limit; counter++){
-            if(p_validate_prime(counter)){
-                prime_numbers=prime_numbers+(counter)+ "";
+            StringBuilder prime_numbers= new StringBuilder();
+            for(int i=1;i<=v_limit;i++) {
+                if(p_validate_prime(i)) {
+                    prime_numbers.append(i).append(", "); // agrega los caracteres al final de un Builder
+                }
             }
-           }
-            return prime_numbers;
+            return prime_numbers.toString();
         }
 
         public static boolean p_validate_prime (int v_number){
@@ -80,14 +84,8 @@ public class Main {
                     numbers_divide++;
                }
             }
-            if (numbers_divide!=2){
-                return false;
-            }
-            return true;
+            return numbers_divide == 2; //retornara aquellos valores que solo tengan dos divisores
         }
-
-
-
 
 
     public static void p_show_info_program(){
