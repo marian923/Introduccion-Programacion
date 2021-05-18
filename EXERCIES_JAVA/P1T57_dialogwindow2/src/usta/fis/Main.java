036f1b6b2e4c3016da49547b456b51ff05e17ad3
Gabriela mora <marian.morag@usantoto.edu.co>
2021-05-17 22:29:20 -0500

    Upload 2021
    Upload 2021

--- EXERCIES_JAVA/P1T57_dialogwindow2/src/usta/fis/Main.java
+++ EXERCIES_JAVA/P1T57_dialogwindow2/src/usta/fis/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	String answer;
-	int amount=0, operation=0, result;
+	int amount, operation, result = 0;
 	int []array;
 	answer= JOptionPane.showInputDialog("Hi, this program is a calculator, "+
             "\n and do it a arithmetic operations, "+
@@ -19,16 +19,44 @@ public class Main {
         for (int i = 0; i < amount; i++) {
             answer = JOptionPane.showInputDialog("Please type the "+(i+1)+" number"); // es i+1 porque como i empieza en 0 se le tiene que ir sumando 1
             array[i]=Integer.valueOf(answer);
+            if(operation==4){
+                if(i>0){
+                    while(array[i]==0){
+                        answer = JOptionPane.showInputDialog(null,"Escriba nuevamente su nombre", "Error! x_x",JOptionPane.ERROR_MESSAGE);
+                        array[i]=Integer.valueOf(answer);
+                    }
+                }
+            }
         }
+
         //llamar al metodo
-        result=calculator(array,operation);
+        result=calculator(array,operation,amount);
         JOptionPane.showMessageDialog(null, "Your result is: "+result) ;
     }
 
-    public static int calculator(int []array, int operation){
-        int result=0;
+    public static int calculator(int []array, int operation, int amount){
 
-
+        int result = array[0];
+        if(operation==1){
+            for (int i = 1; i < amount; i++) {
+                result += array[i];
+            }
+        }
+        if(operation==2){
+            for (int i = 1; i < amount; i++) {
+                result -= array[i];
+            }
+        }
+        if(operation==3){
+            for (int i = 1; i < amount; i++) {
+                result *= array[i];
+            }
+        }
+        if(operation==4){
+            for (int i = 1; i < amount; i++) {
+                result /= array[i];
+            }
+        }
         return result;
 
     }
