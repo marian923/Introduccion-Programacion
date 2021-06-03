204f86187ba8671f512df2dbe50c5af113b2c3bb
Gabriela mora <marian.morag@usantoto.edu.co>
2021-06-03 13:14:29 -0500

    Upload 2021
    Upload 2021

--- EXERCIES_JAVA/P1T61_Triki/src/co/edu/usta/array/triki/logic/Triki.java
+++ EXERCIES_JAVA/P1T61_Triki/src/co/edu/usta/array/triki/logic/Triki.java
@@ -1,26 +1,479 @@
 package co.edu.usta.array.triki.logic;
 
 public class Triki {
-    private char [][] triki;
+    private char[][] triki;
+
+    public void lugaresDelArreglo3() {
+        triki = new char[3][3];
+    }
+
+    public void lugaresDelArreglo5() {
+        triki = new char[5][5];
+    }
+
+    public String inicializarElArreglo() {// es String debido a que no solo es un caracter sino son dos
+        String almacenadorChar = "";
+        for (int i = 0; i < triki.length; i++) {
+            for (int j = 0; j < triki.length; j++) {
+                triki[i][j] = '°'; // es i y es j debido a que estos son los contadores que recorren al arreglo
+                almacenadorChar = almacenadorChar + triki[i][j] + " "; //el char se almacena lo que ha recorrido mas el arreglo y deja un espacio almacenar los char dentro de la variable almacenadorChar que es un String
+            }
+            almacenadorChar = almacenadorChar + "\n"; //es para que cuando termina el for i haga un salto de linea para el for j
+
+        }
+        return almacenadorChar;
+    }
 
 
-    public void lugarArray(){
-        boolean letras=true; // es de tipo boolena ya que solo son dos valores por ende tenemos que evaluar si es verdadero o flaso
-        //son dos for debido a que es un arreglo bidimensional
+    public boolean jugadas(int filasI, int columnasI) { //esta clase sirve para validar la jugada y es de tipo boollean porque solo puede esocger dos valores
+        if (triki[filasI][columnasI] == '°') { // este seria para evaluar los espacios libres que hay si no hay entonces pasa al siguiente proceso
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public char ubicarArregloJ1(int filasI, int columnasI) {
+        if (jugadas(filasI, columnasI)) {
+            triki[filasI][columnasI] = 'X'; // cuando lo vaya a jugar si el resulatdo es verdadero es porque un espacio existe y sera remplazado con X
+        }
+        return 0;
+    }
+
+    public char ubicarArregloJ2(int filasI, int columnasI) {
+        if (jugadas(filasI, columnasI)) {
+            triki[filasI][columnasI] = 'O'; // cuando lo vaya a jugar si el resulatdo es verdadero es porque un espacio existe y sera remplazado con O
+        }
+        return 0;
+    }
+
+    public String presentarTriqui() {
+        String muestraTodoElTriqui = "";
         for (int i = 0; i < triki.length; i++) {
             for (int j = 0; j < triki[0].length; j++) {
-                if (letras==true){
-                    triki[i][j]='X';
-                }else {
-                    triki[i][j]='O';
+                muestraTodoElTriqui = muestraTodoElTriqui + triki[i][j] + " ";
+            }
+            muestraTodoElTriqui = muestraTodoElTriqui + "\n";
+        }
+        return muestraTodoElTriqui;
+    }
+
+    public int verificarSiGanoElJuego3x3(int espacioslibres) {
+        if (triki[0][0] == 'X') { // columna
+            if (triki[0][1] == 'X') {
+                if (triki[0][2] == 'X') {
+                    return  -2;
                 }
             }
-            if(letras==true){
-                letras=false;
-            }else{
-                letras=true;
+        }
+        if (triki[1][0] == 'X') { //columna
+            if (triki[1][1] == 'X') {
+                if (triki[1][2] == 'X') {
+                    return -2;
+                }
             }
         }
+        if (triki[2][0] == 'X') { //columna
+            if (triki[2][1] == 'X') {
+                if (triki[2][2] == 'X') {
+                    return  -2;
+                }
+            }
+        }
+        if (triki[0][0] == 'X') { //fila
+            if (triki[1][0] == 'X') {
+                if (triki[2][0] == 'X') {
+                    return  -2;
+                }
+            }
+        }
+        if (triki[0][1] == 'X') { // fila
+            if (triki[1][1] == 'X') {
+                if (triki[2][1] == 'X') {
+                   return  -2;
+                }
+            }
+        }
+        if (triki[0][2] == 'X') { //fila
+            if (triki[1][2] == 'X') {
+                if (triki[2][2] == 'X') {
+                    return  -2;
+                }
+            }
+        }
+        if (triki[0][0] == 'X') { // diagonal
+            if (triki[1][1] == 'X') {
+                if (triki[2][2] == 'X') {
+                    return  -2;
+                }
+            }
+        }
+        if (triki[0][2] == 'X') { //diagonal
+            if (triki[1][1] == 'X') {
+                if (triki[2][0] == 'X') {
+                    return -2;
+                }
+            }
+        }
+        if (triki[0][0] == 'O') {
+                if (triki[0][1] == 'O') {
+                if (triki[0][2] == 'O') {
+                    return  -3;
+                }
+            }
+        }
+        if (triki[1][0] == 'O') {
+            if (triki[1][1] == 'O') {
+                if (triki[1][2] == 'O') {
+                    return -3;
+                }
+            }
+        }
+        if (triki[2][0] == 'O') {
+            if (triki[2][1] == 'O') {
+                if (triki[2][2] == 'O') {
+                    return  -3;
+                }
+            }
+        }
+        if (triki[0][0] == 'O') {
+            if (triki[1][0] == 'O') {
+                if (triki[2][0] == 'O') {
+                    return  -3;
+                }
+            }
+        }
+        if (triki[0][1] == 'O') {
+            if (triki[1][1] == 'O') {
+                if (triki[2][1] == 'O') {
+                    return -3;
+                }
+            }
+        }
+        if (triki[0][2] == 'O') {
+            if (triki[1][2] == 'O') {
+                if (triki[2][2] == 'O') {
+                    return  -3;
+                }
+            }
+        }
+        if (triki[0][0] == 'O') {
+            if (triki[1][1] == 'O') {
+                if (triki[2][2] == 'O') {
+                    return  -3;
+                }
+            }
+        }
+        if (triki[0][2] == 'O') {
+            if (triki[1][1] == 'O') {
+                if (triki[2][0] == 'O') {
+                    return -3;
+                }
+            }
+        }
+        return espacioslibres;
+
+    }
+
+    public int verificarSiGanoElJuego5x5(int espacioslibres) {
+        if (triki[0][0] == 'X') { //columna
+            if (triki[0][1] == 'X') {
+                if (triki[0][2] == 'X') {
+                    if (triki[0][3] == 'X') {
+                        if (triki[0][4] == 'X') {
+                            return  -2;
+                        }
+                    }
+                }
+            }
+            if (triki[0][0] == 'X') { //fila
+                if (triki[1][0] == 'X') {
+                    if (triki[2][0] == 'X') {
+                        if (triki[3][0] == 'X') {
+                            if (triki[4][0] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[0][0] == 'X') { //diagonal
+                if (triki[1][1] == 'X') {
+                    if (triki[2][2] == 'X') {
+                        if (triki[3][3] == 'X') {
+                            if (triki[4][4] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[0][4] == 'X') { //fila
+                if (triki[1][4] == 'X') {
+                    if (triki[2][4] == 'X') {
+                        if (triki[3][4] == 'X') {
+                            if (triki[4][4] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[0][4] == 'X') { //diagonal
+                if (triki[1][3] == 'X') {
+                    if (triki[2][2] == 'X') {
+                        if (triki[3][1] == 'X') {
+                            if (triki[4][0] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[1][0] == 'X') { //columna
+                if (triki[1][1] == 'X') {
+                    if (triki[1][2] == 'X') {
+                        if (triki[1][3] == 'X') {
+                            if (triki[1][4] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[2][0] == 'X') { //columna
+                if (triki[2][1] == 'X') {
+                    if (triki[2][2] == 'X') {
+                        if (triki[2][3] == 'X') {
+                            if (triki[2][4] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[3][0] == 'X') { //columna
+                if (triki[3][1] == 'X') {
+                    if (triki[3][2] == 'X') {
+                        if (triki[3][3] == 'X') {
+                            if (triki[3][4] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[4][0] == 'X') { //columna
+                if (triki[4][1] == 'X') {
+                    if (triki[4][2] == 'X') {
+                        if (triki[4][3] == 'X') {
+                            if (triki[4][4] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[0][1] == 'X') { //fila
+                if (triki[1][1] == 'X') {
+                    if (triki[2][1] == 'X') {
+                        if (triki[3][1] == 'X') {
+                            if (triki[4][1] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[0][2] == 'X') { //fila
+                if (triki[1][2] == 'X') {
+                    if (triki[2][2] == 'X') {
+                        if (triki[3][2] == 'X') {
+                            if (triki[4][2] == 'X') {
+                                return  -2;
+                            }
+                        }
+                    }
+                }
+            }
+            if (triki[0][3] == 'X') { //fila
+                if (triki[1][3] == 'X') {
+                    if (triki[2][3] == 'X') {
+                        if (triki[3][3] == 'X') {
+                            if (triki[4][3] == 'X') {
+                                return -2;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (triki[0][0] == 'O') {
+                if (triki[0][1] == 'O') {
+                    if (triki[0][2] == 'O') {
+                        if (triki[0][3] == 'O') {
+                            if (triki[0][4] == 'O') {
+                                return -3;
+                            }
+                        }
+                    }
+                }
+                if (triki[0][0] == 'O') {
+                    if (triki[1][0] == 'O') {
+                        if (triki[2][0] == 'O') {
+                            if (triki[3][0] == 'O') {
+                                if (triki[4][0] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[0][0] == 'O') {
+                    if (triki[1][1] == 'O') {
+                        if (triki[2][2] == 'O') {
+                            if (triki[3][3] == 'O') {
+                                if (triki[4][4] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[0][4] == 'O') {
+                    if (triki[1][4] == 'O') {
+                        if (triki[2][4] == 'O') {
+                            if (triki[3][4] == 'O') {
+                                if (triki[4][4] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[0][4] == 'O') {
+                    if (triki[1][3] == 'O') {
+                        if (triki[2][2] == 'O') {
+                            if (triki[3][1] == 'O') {
+                                if (triki[4][0] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[1][0] == 'O') {
+                    if (triki[1][1] == 'O') {
+                        if (triki[1][2] == 'O') {
+                            if (triki[1][3] == 'O') {
+                                if (triki[1][4] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[2][0] == 'O') {
+                    if (triki[2][1] == 'O') {
+                        if (triki[2][2] == 'O') {
+                            if (triki[2][3] == 'O') {
+                                if (triki[2][4] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[3][0] == '0') {
+                    if (triki[3][1] == '0') {
+                        if (triki[3][2] == '0') {
+                            if (triki[3][3] == '0') {
+                                if (triki[3][4] == '0') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[4][0] == '0') {
+                    if (triki[4][1] == '0') {
+                        if (triki[4][2] == '0') {
+                            if (triki[4][3] == '0') {
+                                if (triki[4][4] == '0') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[0][1] == 'O') {
+                    if (triki[1][1] == 'O') {
+                        if (triki[2][1] == 'O') {
+                            if (triki[3][1] == 'O') {
+                                if (triki[4][1] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[0][2] == 'O') {
+                    if (triki[1][2] == 'O') {
+                        if (triki[2][2] == 'O') {
+                            if (triki[3][2] == 'O') {
+                                if (triki[4][2] == 'O') {
+                                    return -3;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (triki[0][3] == 'O') {
+                    if (triki[1][3] == 'O') {
+                        if (triki[2][3] == 'O') {
+                            if (triki[3][3] == 'O') {
+                                if (triki[4][3] == 'O') {
+                                    return -2;
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            }
+
+        }
+        return espacioslibres;
+
+    }
+
+    public int mostrarTriquiO(){
+        int contarcantidadesdeO=0;
+        for (int i = 0; i < triki.length; i++) {
+            for (int j = 0; j < triki[0].length; j++) {
+                if(triki[i][j]=='O'){
+                    contarcantidadesdeO++;
+                }
+
+
+            }
+
+        }
+        return contarcantidadesdeO;
+    }
+    public int mostrarTriquiX(){
+        int contarcantidadesdeX=0;
+        for (int i = 0; i < triki.length; i++) {
+            for (int j = 0; j < triki[0].length; j++) {
+                if(triki[i][j]=='X'){
+                    contarcantidadesdeX++;
+                }
+            }
+
+        }
+        return contarcantidadesdeX;
     }
 
 }
--- EXERCIES_JAVA/P1T61_Triki/src/co/edu/usta/array/triki/view/FormView.java
+++ EXERCIES_JAVA/P1T61_Triki/src/co/edu/usta/array/triki/view/FormView.java
@@ -8,12 +8,98 @@ public class FormView {
     private static Triki myTriki;
     public static void main(String[] params){
         myTriki=new Triki();
-        int opcion;
-        String option;
+        int opcion,filasI=0,columnasI=0, espacioslibres = 0, espacioslibresfinal=0;
+        char triki;
+        String option,filasS,columnasS;
         option= JOptionPane.showInputDialog("Bienvenido a la empresa Takis, te vamos a ofrecer el juego de triki, " +
-                "\n si digitas la opcion 1 es para 3x3, si digitas la opcion 2 es para 5x5");
+                "\nsi digitas la opcion 0 es para salir del juego," +
+                "\nsi digitas la opcion 1 es para jugar 3x3, " +
+                "\nsi digitas la opcion 2 es para jugar 5x5");
         opcion=Integer.parseInt(option);
-        myTriki.lugarArray();
+        while(opcion>2){
+            JOptionPane.showMessageDialog(null,"El numero ingresado es incorrecto ", "Error", JOptionPane.ERROR_MESSAGE);
+            option=JOptionPane.showInputDialog("Digite el numero nuevamente");
+            opcion=Integer.parseInt(option);
+        }
+        if(opcion==1){
+            espacioslibres=9;//
+            myTriki.lugaresDelArreglo3();
+        }
+        if (opcion==2){
+            espacioslibres=25;
+            myTriki.lugaresDelArreglo5();
+        }
+        JOptionPane.showMessageDialog(null,myTriki.inicializarElArreglo() );
+        do { // sirve para que los jugadores digiten sus movimientos, mientras que los espaciosvacios o como tal la clase validarjuego sea mayor a 0 va a seguir ejecutandose
+
+            if (espacioslibres>0){ // si hay mas de 0 espacios libresse peude seguir jugando
+                filasS=JOptionPane.showInputDialog(null,"Jugador X por favor digite la fila en que desea poner su jugada ");
+                filasI=Integer.parseInt(filasS);
+                columnasS=JOptionPane.showInputDialog(null, "Jugador X por favor digite la columna en que desea poner su jugada");
+                columnasI=Integer.parseInt(columnasS);
+                while(myTriki.jugadas(filasI,columnasI)==false){ // si la opcion es falsa este no permitiria seguir hasta ingrese un valor verdadero, tabien sirve para cuando el numero digitado sea en la misma posicion que el de otro jugado no se pueda ejecutar
+                    JOptionPane.showMessageDialog(null," Jugador X los valores ingresados son incorrectos  ","ERR",JOptionPane.INFORMATION_MESSAGE);
+                    filasS=JOptionPane.showInputDialog(null, "Digitelo nuevamente la fila ");
+                    filasI=Integer.parseInt(filasS);
+                    columnasS = JOptionPane.showInputDialog(null, "Digite nuevamente la columna");
+                    columnasI=Integer.parseInt(columnasS);
+                }
+                myTriki.ubicarArregloJ1(filasI,columnasI);
+                espacioslibres--;// espacios libres es igual al profegrama para identificar cuantos espacios
+                espacioslibresfinal=espacioslibres; //
+
+            }
+            JOptionPane.showMessageDialog(null,myTriki.presentarTriqui());
+            if(opcion ==1){
+                espacioslibres=myTriki.verificarSiGanoElJuego3x3(espacioslibres);
+            }
+            if(opcion ==2){
+                espacioslibres=myTriki.verificarSiGanoElJuego5x5(espacioslibres);
+            }
+
+
+            if (espacioslibres>0){
+                filasS=JOptionPane.showInputDialog(null,"Jugador O por favor digite la fila en que desea poner su jugada ");
+                filasI=Integer.parseInt(filasS);
+                columnasS=JOptionPane.showInputDialog(null, "Jugador O por favor digite la columna en que desea poner su jugada");
+                columnasI=Integer.parseInt(columnasS);
+                while(myTriki.jugadas(filasI,columnasI)==false){
+                    JOptionPane.showMessageDialog(null,"Jugador O los valores ingresados son incorrectos ","ERR",JOptionPane.INFORMATION_MESSAGE);
+                    filasS=JOptionPane.showInputDialog(null, "Digitelo nuevamente la fila ");
+                    filasI=Integer.parseInt(filasS);
+                    columnasS = JOptionPane.showInputDialog(null, "Digite nuevamente la columna");
+                    columnasI=Integer.parseInt(columnasS);
+                }
+                myTriki.ubicarArregloJ2(filasI,columnasI);
+
+                espacioslibres--;
+                espacioslibresfinal=espacioslibres; // e.final nunca cambia como si lo es espacioslibres
+            }
+            JOptionPane.showMessageDialog(null, myTriki.presentarTriqui());; //  se le va restando cada vez a los espacios libres ,se hace este proceso debido a que di los espacios son difrentes a O alida los if cada vez que esta jugando hace el cambio
+        }while(espacioslibres>0); // es mayor que = porque cunaod obtenga valores de 0 o menores uqe 0 es porque se termino el juego o alguien perdio
+
+
+        if(espacioslibres ==0){ //se acaban los espacios libres y nadie gano
+            JOptionPane.showMessageDialog(null, "Ninguno de los jugadores gano"+
+                    "\n Los espacios libres son : "+ espacioslibresfinal + " Cantidades de O fueron:"+myTriki.mostrarTriquiO()+" y cantidades de X digitadas fueron"+myTriki.mostrarTriquiX());
+
+
+        }
+        if (espacioslibres == -2){
+            JOptionPane.showMessageDialog(null, "Jugador X ha ganado"+
+                    "\n Los espacios libres son : "+ espacioslibresfinal + " Cantidades de O fueron :"+myTriki.mostrarTriquiO()+" y cantidades de X digitadas fueron: "+myTriki.mostrarTriquiX());
+        }
+        if (espacioslibres == -3){
+            JOptionPane.showMessageDialog(null, "Jugador O ha ganado"+
+                    "\n Los espacios libres son : "+ espacioslibresfinal+ " Cantidades de O fueron :"+myTriki.mostrarTriquiO()+" y cantidades de X digitadas fueron: "+myTriki.mostrarTriquiX());
+            
+        }
+
+
+
+
+
+
 
     }
 }
